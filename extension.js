fdb1fbdb27327518ed7fcbb2ab80a3ed9dfcddbf
Farid Zelli <82771409+FaridZelli@users.noreply.github.com>
2023-07-03 22:55:41 +0330

    Update extension.js

--- extension.js
+++ extension.js
@@ -2,51 +2,23 @@ const St = imports.gi.St;
 const Main = imports.ui.main;
 const GLib = imports.gi.GLib;
 
-function powerOff() {
-    try {
-        var value = GLib.spawn_command_line_async('gnome-session-quit --power-off');
-    } catch (err) {
-        Main.notify(err + ": gnome-extension-poweroff-button@InkaAlicja experienced an error");
-        log("gnome-extension-poweroff-button@InkaAlicja:" + err);
-    }
-}
-
 function suspend() {
     try {
         var value = GLib.spawn_command_line_async('systemctl suspend');
     } catch (err) {
-        Main.notify(err + ": gnome-extension-poweroff-button@InkaAlicja experienced an error");
-        log("gnome-extension-poweroff-button@InkaAlicja:" + err);
+        Main.notify(err + ": tap-to-sleep@FaridZelli experienced an error");
+        log("tap-to-sleep@FaridZelli:" + err);
     }
 }
 
-let powerOffButton;
-let powerOffIcon;
 let suspendIcon;
 let suspendButton;
-
-let powerOffConnection;
 let suspendConnection;
 
 function init () {}
 
 function enable () {
-    powerOffIcon = new St.Icon({
-        icon_name: 'system-shutdown-symbolic',
-        style_class: 'system-status-icon'
-    });
-
-    powerOffButton = new St.Bin({
-        style_class: 'panel-button',
-        reactive: true,
-        can_focus: true,
-        track_hover: true
-    });
-
-    powerOffButton.set_child(powerOffIcon);
-    powerOffConnection = powerOffButton.connect('button-press-event', powerOff);
-
-    suspendIcon = new St.Icon({
+suspendIcon = new St.Icon({
         icon_name: 'weather-clear-night-symbolic',
         style_class: 'system-status-icon'
     });
@@ -60,28 +32,15 @@ function enable () {
 
     suspendButton.set_child(suspendIcon);
     suspendConnection = suspendButton.connect('button-press-event', suspend);
+    suspendConnection = suspendButton.connect('touch-event', suspend);
 
     Main.panel._rightBox.insert_child_at_index(suspendButton, -1);
-    Main.panel._rightBox.insert_child_at_index(powerOffButton, -1);
 }
 
 function disable() {
     Main.panel._rightBox.remove_child(suspendButton);
-    Main.panel._rightBox.remove_child(powerOffButton);
 
-    if(powerOffIcon) {
-        powerOffIcon.destroy();
-        powerOffIcon = null;
-    }
-    if(powerOffButton) {
-        if(powerOffConnection){
-            powerOffButton.disconnect(powerOffConnection);
-            powerOffConnection = null;
-        }
-        powerOffButton.destroy();
-        powerOffButton = null;
-    }
-    if(suspendIcon) {
+if(suspendIcon) {
         suspendIcon.destroy();
         suspendIcon = null;
     }
